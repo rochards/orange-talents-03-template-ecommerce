b1922bff7d4f038c7b4686a8dc439099cf148d8f
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-03-25 19:15:56 -0300

    Add Spring Security
      Foi preciso add o Spring Security para utilizar a classe BCryptPasswordEncoder. Essa classe possui um metodo para fazer a criptografia da senha.
    
    - Add dependencia do Spring Security no pom.xml;
    - Add classe SecurityConfiguration com sobrecarga do metodo de
      autorizacao
    - Add criptografia da senha no construtor da classe Usuario.

---
+++ src/main/java/br/com/zupacademy/desafiomercadolivre/config/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package br.com.zupacademy.desafiomercadolivre.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().csrf().disable();
+        /** Eu preciso desabilitar o csrf para o POST funcionar, pq ele nao eh Idempotente, ou seja, ele modifica o
+         * estado da aplicacao servidora.
+         * SPRING DOC -> Our recommendation is to use CSRF protection for any request that could be processed by a
+         * browser by normal users. If you are only creating a service that is used by non-browser clients,
+         * you will likely want to disable CSRF protection */
+    }
+}
--- src/main/java/br/com/zupacademy/desafiomercadolivre/domain/usuario/Usuario.java
+++ src/main/java/br/com/zupacademy/desafiomercadolivre/domain/usuario/Usuario.java
@@ -1,5 +1,7 @@
 package br.com.zupacademy.desafiomercadolivre.domain.usuario;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.persistence.*;
 import java.time.OffsetDateTime;
 
@@ -24,7 +26,7 @@ public class Usuario {
 
     public Usuario(String login, String senha) {
         this.login = login;
-        this.senha = senha;
+        this.senha = new BCryptPasswordEncoder().encode(senha);
         this.criadoEm = OffsetDateTime.now();
     }
 
