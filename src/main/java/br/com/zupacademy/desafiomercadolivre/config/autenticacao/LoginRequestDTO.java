22ab71c1344455881d7be843298c4390afb9ccb0
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-03-30 14:33:49 -0300

    Implementa endpoint de autenticacao
    - Add LoginController;
    - Add LoginRequestDTO com validacoes nos campos.

---
+++ src/main/java/br/com/zupacademy/desafiomercadolivre/config/autenticacao/LoginRequestDTO.java
@@ -0,0 +1,24 @@
+package br.com.zupacademy.desafiomercadolivre.config.autenticacao;
+
+import br.com.zupacademy.desafiomercadolivre.domain.usuario.Usuario;
+import br.com.zupacademy.desafiomercadolivre.errors.validators.ExistsValue;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequestDTO {
+
+    @NotBlank @Email
+    @ExistsValue(message = "não há usuário cadastrado com o e-mail informado", domainClass = Usuario.class,
+            fieldName = "login")
+    private String login;
+
+    @NotBlank @Length(min = 6)
+    private String senha;
+
+    public LoginRequestDTO(@NotBlank @Email String login, @NotBlank @Length(min = 6) String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+}
