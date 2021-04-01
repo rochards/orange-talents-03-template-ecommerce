02e41be9a78775cf33a79ebdf1878f90b6abf567
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-04-01 16:33:15 -0300

    Implementa recebimento de arquivos
    - Refatora classe ImagensProdutoRequestDTO. Add atributo que recebe os
      arquivos;
    - Refatora classe NovaImagemProdutoController.

--- src/main/java/br/com/zupacademy/desafiomercadolivre/domain/produto/ImagemProdutoRequestDTO.java
+++ src/main/java/br/com/zupacademy/desafiomercadolivre/domain/produto/ImagemProdutoRequestDTO.java
@@ -1,47 +1,17 @@
 package br.com.zupacademy.desafiomercadolivre.domain.produto;
 
-import br.com.zupacademy.desafiomercadolivre.domain.usuario.Usuario;
-import br.com.zupacademy.desafiomercadolivre.errors.validators.ExistsValue;
-import org.springframework.util.Assert;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.persistence.EntityManager;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.util.Optional;
+import javax.validation.constraints.Size;
+import java.util.List;
 
 public class ImagemProdutoRequestDTO {
 
-    @NotNull
-    @ExistsValue(message = "não há produto cadastrado com o id informado", domainClass = Produto.class, fieldName = "id")
-    private Integer produtoId;
+    @NotNull @Size(min = 1)
+    private List<MultipartFile> imagens;
 
-    @NotBlank
-    private String link;
-
-    public ImagemProdutoRequestDTO(@NotNull Integer produtoId, @NotBlank String link) {
-        this.produtoId = produtoId;
-        this.link = link;
-    }
-
-    public Optional<ImagemProduto> toModel(EntityManager em, Usuario usuario) {
-        var produto = buscaProduto(em, produtoId).get();
-
-        if (produtoPertenceAUsuario(produto, usuario)) {
-            return Optional.of(new ImagemProduto(link, produto));
-        }
-
-        return Optional.empty();
-    }
-
-    private Optional<Produto> buscaProduto(EntityManager em, Integer id) {
-        Assert.notNull(id, "o id não deveria vir nulo");
-        return Optional.ofNullable(em.find(Produto.class, id));
-    }
-
-    private boolean produtoPertenceAUsuario(Produto produto, Usuario usuario) {
-        var usuarioId = usuario.getId();
-        var donoProdutoId = produto.getDono().getId();
-
-        return usuarioId.equals(donoProdutoId);
+    public ImagemProdutoRequestDTO(@Size(min = 1) List<MultipartFile> imagens) {
+        this.imagens = imagens;
     }
 }
--- src/main/java/br/com/zupacademy/desafiomercadolivre/domain/produto/NovaImagemProdutoController.java
+++ src/main/java/br/com/zupacademy/desafiomercadolivre/domain/produto/NovaImagemProdutoController.java
@@ -2,21 +2,17 @@ package br.com.zupacademy.desafiomercadolivre.domain.produto;
 
 import br.com.zupacademy.desafiomercadolivre.domain.usuario.Usuario;
 import br.com.zupacademy.desafiomercadolivre.errors.handlers.APIErrorHandler;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("produtos/imagens")
+@RequestMapping("produtos")
 public class NovaImagemProdutoController {
 
     private final EntityManager em;
@@ -25,22 +21,15 @@ public class NovaImagemProdutoController {
         this.em = em;
     }
 
-    @PostMapping
     @Transactional
-    public ResponseEntity<?> cadastra(@RequestBody @Valid ImagemProdutoRequestDTO imagemRequest, BindingResult result,
-        @AuthenticationPrincipal Usuario usuario) {
+    @PostMapping("{id}/imagens")
+    public ResponseEntity<?> adiciona(@PathVariable Integer id, @Valid ImagemProdutoRequestDTO imagemRequest,
+        BindingResult result, @AuthenticationPrincipal Usuario usuario) {
 
         if (result.hasErrors()) {
             return ResponseEntity.badRequest().body(new APIErrorHandler(result.getFieldErrors()));
         }
 
-        var optImagemProduto = imagemRequest.toModel(em, usuario);
-        if (optImagemProduto.isPresent()) {
-            em.persist(optImagemProduto.get());
-
-            return ResponseEntity.ok().build();
-        }
-
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        return ResponseEntity.ok().build();
     }
 }
